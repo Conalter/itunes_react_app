b3ab0bef2b92d77a7dc8a7d9e6c1260a8bb9e61d
Lyle Mitchell <ljwmitchell@gmail.com>
2018-06-27 13:40:09 +0100

    adds container file & basic class

---
+++ src/containers/ItunesContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ItunesContainer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      songs: [],
+    };
+  }
+  
+}
+
+export default ItunesContainer;
